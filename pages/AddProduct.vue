9d4a843b260e835134db7aa897533ae82861f671
PedramAlidust <karsite1234@gmail.com>
2023-01-02 19:42:23 +0330

    update id

--- pages/AddProduct.vue
+++ pages/AddProduct.vue
@@ -75,7 +75,7 @@
               <p class="FormLabel fw-bold">Type Switcher</p>
             </div>
             <div>
-              <select @change="TypeSwitcher($event)" id="#ProductType" class="ms-3 form-select">
+              <select @change="TypeSwitcher($event)" id="ProductType" class="ms-3 form-select">
                 <option selected>select a product type</option>
                 <option value="DVD" id="DVD">DVD</option>
                 <option value="Furniture" id="Furniture">Furniture</option>
